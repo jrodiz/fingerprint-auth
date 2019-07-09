d957f08741f73bc1e9b6275830e522bf888e1761
Joseph Rodiz <joseph.rodiz@neoris.com>
2019-07-09 15:15:39 -0500

    Initial commit

---
+++ biometric-auth/src/main/java/com/josephrodiz/biometric/BiometricCallback.java
@@ -0,0 +1,25 @@
+package com.josephrodiz.biometric;
+
+public interface BiometricCallback {
+
+    void onSdkVersionNotSupported();
+
+    void onBiometricAuthenticationNotSupported();
+
+    void onBiometricAuthenticationNotAvailable();
+
+    void onBiometricAuthenticationPermissionNotGranted();
+
+    void onBiometricAuthenticationInternalError(String error);
+
+
+    void onAuthenticationFailed();
+
+    void onAuthenticationCancelled();
+
+    void onAuthenticationSuccessful();
+
+    void onAuthenticationHelp(int helpCode, CharSequence helpString);
+
+    void onAuthenticationError(int errorCode, CharSequence errString);
+}
